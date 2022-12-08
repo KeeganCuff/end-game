d9d22d9ca3f7738688842bdc07d0cd96bc981b5c
KeeganCuff <keegancuff@gmail.com>
2022-12-08 15:41:04 -0600

    Baseline empty mod

---
+++ src/main/java/net/keegancuff/endgame/EndGame.java
@@ -0,0 +1,16 @@
+package net.keegancuff.endgame;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndGame implements ModInitializer {
+	public static final String MOD_ID = "endgame";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/keegancuff/endgame/EndGameClient.java
@@ -0,0 +1,10 @@
+package net.keegancuff.endgame;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndGameClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
